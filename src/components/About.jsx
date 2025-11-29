3cd27617a9031d91fa8588a11606f3b1cb002558
APURV SINHA <119706011+xeylous@users.noreply.github.com>
2025-11-29 23:38:03 +0530

    Change photo import from JPG to PNG format

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { motion } from "framer-motion";
 import Image from "next/image";
-import { Photo } from "../../public/Photo.jpg";
+import { Photo } from "../../public/Photo.png";
 
 export default function AboutMe() {
   return (
